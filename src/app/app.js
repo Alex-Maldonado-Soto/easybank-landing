5f3c665b64901ed6bb98d3b2efdc62e6e737faa2
maldexander <alexandermaldonadosoto@gmail.com>
2023-03-15 21:26:43 -0600

    node con pug y sass instalados y funcionando correctamente

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,16 @@
 const express = require("express")
 
+const morgan = require("morgan")
+
 const app = express()
+const static = __dirname.replace("app", "public")
 
 app.set("port", process.env.PORT || 5500)
 app.set("views", "./src/public/view")
 app.set("view engine", "pug")
+
+app.use(morgan("dev"))
+app.use(express.static(static))
 
 
 app.get("/", (req, res) => {
